96ddf85365f13fa3dd00624243001d850d53517b
Aditya Bhartiya <91467441+AdityaJB@users.noreply.github.com>
2022-10-05 23:42:14 +0530

    Update and rename p.java to Bankers and Stocks .java

--- Bankers and Stocks .java
+++ Bankers and Stocks .java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class p{
+class Solution{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Long n = sc.nextLong();
