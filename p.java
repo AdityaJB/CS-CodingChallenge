765f8826ce6419e8c5be3b8835b743e7a5f221c1
Aditya Bhartiya <91467441+AdityaJB@users.noreply.github.com>
2022-10-05 23:41:10 +0530

    Add files via upload

---
+++ p.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class p{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long n = sc.nextLong();
+        int sqt = (int)Math.sqrt(n);
+        int e = 0, o = 0;
+        for(int i=1;i<=sqt;i++){
+            if(n%i == 0){
+                
+                if(i%2 == 0)e++;
+                else o++;
+                if((n/i)%2 == 0)e++;
+                else o++;
+                if(i == n/i && i%2==0)e--;
+                if(i == n/i && i%2!=0)o--;
+
+            }
+        }
+        if(e == o) System.out.println("PASS");
+        else if(e>o)System.out.println("BUY");
+        else System.out.println("SELL");
+        sc.close();
+    }
+}
